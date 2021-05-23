64643956dbff03e1cc4cb18197183af567397a67
Jonathan Amador Lorence <jonathan.amadorlorence@gmail.com>
2021-05-23 23:33:36 +0200

    Formatting the code
    Formatting the code and checking misspell

--- tictactoe.py
+++ tictactoe.py
@@ -3,10 +3,10 @@ import pyinputplus
 import datetime
 import threading
 #Goal of this project is to create a game that is one player can play with the computer.
-
-#create a dictionary with the position in the table
-
 class Game:
+    """
+    This class contain, information from the player, table
+    """
     
     def __init__(self,game,simbol,player,position,table):
         self.simbol=simbol
@@ -14,10 +14,10 @@ class Game:
         self.player=player
         self.table=table
         self.game=game
+    
     def print_Table(self):
         """
-        Print the table to the fscreen
-
+        Print the table to the screen
         """
         print("welcome to tic toe")
         print(self.table['top-left']+"     |       "+self.table["top-middle"]+"      |    "+self.table['top-right'])
@@ -27,15 +27,18 @@ class Game:
         print(self.table['button-left']+"     |        "+self.table["button-middle"]+"     |   "+self.table["button-right"])
 
     def printing_Val(self,val):
+        """
+        this function will find, respect to sign , who is the winner either pc or player
+        """
         if self.simbol==val:
             print(self.player+" won!!")
         else:
             print("PC wont")
         return "finish"
-#winning
+
     def checkingwinner(self):
       """
-      this function check for the winner, when the game is wont.
+      this function check for the winner, when the game is won,part need to be improved.
       """
        
       if (self.table['top-left']==self.table["top-middle"])&(self.table["top-middle"]==self.table["top-middle"]):
@@ -55,26 +58,27 @@ class Game:
             
 def playing_with_Pc(gameInfo):
     """
-    PC will play with the player
+    This function will assign to the pc the sign that pc will use.
     """
     if gameInfo.simbol=="X" :
         pc_simbol="O"
     else:
         pc_simbol="X"
     return pc_simbol
+
 def pc_choosing_position(position_list):
         """
-        Generating random_numbers
+        This will produce a random.move ment from pc, and also will check if this place to move is empty or not
         """
         max_ind=len(position_list)
-        pc_choose=random.randint(0,max_ind-1)
         while True:
+            pc_choose=random.randint(0,max_ind-1)
             if position_list[pc_choose]!="":
                 return position_list[pc_choose]
 
 def start_Game():
     """
-    Function will start the game, and print the possible position where that be chosen
+    Function will start the game, print the positions on the table.
     """
     table={'top-left':"","top-middle":"","top-right":"",
            'middle-left':"","center":"","middle-right":"",
@@ -96,7 +100,7 @@ def start_Game():
        game.table[pc_choosing_position(position_1)]=playing_with_Pc(game)
        game.print_Table()
        
-    #creating game
+    #After the first input until the game has fishihed
     while True :
         value_forpc=pc_choosing_position(position_1)
         position_user=pyinputplus.inputMenu(position_1, lettered=True, numbered=False)
@@ -110,7 +114,7 @@ def start_Game():
             print(game.checkingwinner())
         counter=+1
             
-print("Wlcome to tic-toe-game")
+print("Welcome to tic-toe-game")
 print("")
 print("top-left        |       top-middle        |      top-right")
 print("-----------------------------------------------------------------")
@@ -118,7 +122,7 @@ print("middle-left     +       center            +      middle-right")
 print("-----------------------------------------------------------------")
 print("button-left     |        button-middle    |      button-right")
 print("")
-
+start_Game()
 
       
 
